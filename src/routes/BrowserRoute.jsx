a80f964f6c11a982ab777477ee29f5cac10ecd5e
shivam <shivam520p@gmail.com>
2024-12-14 18:31:32 +0530

    Simple Chamge

--- src/routes/BrowserRoute.jsx
+++ src/routes/BrowserRoute.jsx
@@ -7,7 +7,7 @@ import About from "../components/about/About";
 import TutorSection from "../components/TutorSection/TutorSection";
 import Contact from "../components/contact/Contact";
 import { FindTutor } from "../components/findtutor/FindTutor";
-import GetStarted from "../components/user/GetStarted"
+import GetStarted from "../components/user/GetStarted";
 import Register from "../components/user/Register";
 import AllCourse from "../components/courses/AllCourse";
 import AllLanguage from "../components/courses/AllLanguage";
@@ -19,20 +19,22 @@ export const BrowserRoute = () => {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HeroSection />} />
-        <Route path="/findtutor" element={<FindTutor />} />
-        <Route path="/instructordetails" element={<TutorDetails />} />
-        <Route path="/courses" element={<AllCourse />} />
-        <Route path="/courses/languages" element={<AllLanguage />} />
-        <Route path="/courses/subjects" element={<AllSubjects />} />
-        <Route path="/courses/programming" element={<Programming />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/tutorsection" element={<TutorSection />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<GetStarted/>}/>
-        <Route path="/register" element={<Register/>}/>
-      </Routes>
+      <main className="px-4 lg:px-0">
+        <Routes>
+          <Route path="/" element={<HeroSection />} />
+          <Route path="/findtutor" element={<FindTutor />} />
+          <Route path="/instructordetails" element={<TutorDetails />} />
+          <Route path="/courses" element={<AllCourse />} />
+          <Route path="/courses/languages" element={<AllLanguage />} />
+          <Route path="/courses/subjects" element={<AllSubjects />} />
+          <Route path="/courses/programming" element={<Programming />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/tutorsection" element={<TutorSection />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<GetStarted />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
       <Footer />
     </>
   );
